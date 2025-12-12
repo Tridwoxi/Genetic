0e76248ed2a9f52b50275c6a97ae10553723a5f8
Tridwoxi <dw.oxidane@gmail.com>
2025-12-12 03:10:37 -0500

    Add component base and some components.

--- src/genetic.py
+++ src/genetic.py
@@ -8,13 +8,16 @@ Requires python>=3.12.
 from __future__ import annotations
 
 import sys
+from abc import ABC
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from dataclasses import dataclass
-from random import choices, randint, seed
+from random import choice, choices, randint, seed
 from time import time
-from typing import Callable, ClassVar, override
+from types import NoneType
+from typing import Callable, ClassVar, Protocol, Self, override, runtime_checkable
 
 # ruff: noqa: T201 S311
+# TODO: remove parameter class, shove into context
 
 __all__: list[str] = []  # This module is not meant to be imported.
 
@@ -29,17 +32,64 @@ class Context:
     seed: ClassVar[int | None] = None
     trials: ClassVar[int] = 1
 
+    @staticmethod
+    def validate() -> bool:
+        if Context.dimensions <= 0:
+            print(f"Dimensions is {Context.dimensions}, but must be strictly positive.")
+            return False
+        if Context.granularity <= 0:
+            granularity = Context.granularity
+            print(f"Granularity is {granularity}, but must be strictly positive.")
+            return False
+        if Context.trials <= 0:
+            print(f"Trials is {Context.trials}, but must be strictly positive.")
+            return False
+        return True
+
 
 def debug(message: str) -> None:
     if Context.debug:
+        print("\t", end="", file=sys.stderr)
         print(message, file=sys.stderr)
+
+
+## Data base class. ####################################################################
+
+
+@runtime_checkable
+class Named(Protocol):
+    __name__: str
+
+
+@dataclass(frozen=True, slots=True)
+class Component[T](ABC):
+
+    name: str
+    data: T
+
+    @override
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}[{self.name}]"
+
+    @classmethod
+    def of(cls, data: T, name: str | None = None) -> Self:
+        if name is None:
+            name = data.__name__ if isinstance(data, Named) else "Unnamed"
+            name = name.replace("_", " ").title().replace("", "").strip()
+        return cls(name, data)
+
+    @staticmethod
+    def to[S](target: type[Component[S]]) -> Callable[[S], Component[S]]:
+        def wrapper(data: S) -> Component[S]:
+            return target.of(data)
+
+        return wrapper
 
 
 ## Data model. #########################################################################
 
 
-@dataclass(frozen=True)
-class State:
+class State(Component[tuple[int, ...]]):
     """Point in a state space.
 
     Sequence of length Context.dimensions, each element of which is greater than or
@@ -48,25 +98,13 @@ class State:
     is unhelpful for this demonstration. Analgously, an organism.
     """
 
-    val: tuple[int, ...]
-
     @staticmethod
     def random() -> State:
         gen = (randint(0, Context.granularity) for _ in range(Context.dimensions))
-        return State(tuple(gen))
+        return State.of(tuple(gen))
 
 
-@dataclass(frozen=True)
-class _Named:
-    name: str
-
-    @override
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__}[{self.name.replace("\n", " ")}]"
-
-
-@dataclass(frozen=True)
-class Recombinator(_Named):
+class Recombinator(Component[Callable[[State, State], State]]):
     """Combine two parent states to produce a sucessor.
 
     Genetic algorithms are blind to the nature of problems, so do not intelligently
@@ -74,30 +112,16 @@ class Recombinator(_Named):
     themselves is available to the recombinator. Analogously, the mating procedure.
     """
 
-    fn: Callable[[State, State], State]
 
-    @staticmethod
-    def null(mother: State, _: State) -> State:
-        return mother
-
-
-@dataclass(frozen=True)
-class Mutator(_Named):
+class Mutator(Component[Callable[[State], State]]):
     """Edit a state.
 
     This introduces diversity into the environment, in contrast to the recombinator,
     which only shuffles portions around. Analogously, genetic drift.
     """
 
-    fn: Callable[[State], State]
 
-    @staticmethod
-    def null(state: State) -> State:
-        return state
-
-
-@dataclass(frozen=True)
-class Fitness(_Named):
+class Fitness(Component[Callable[[State], float]]):
     """Evaluate a state.
 
     Defining a fitness function is how you and I can define the problem to solve. This
@@ -105,30 +129,16 @@ class Fitness(_Named):
     more than an organism's state in actual enviornments, I will not model that.
     """
 
-    fn: Callable[[State], float]
 
-    @staticmethod
-    def null(_: State) -> float:
-        return 1.0
-
-
-@dataclass(frozen=True)
-class GoalTest(_Named):
+class GoalTest(Component[Callable[[State], bool]]):
     """If a state is sufficiently optimal.
 
     Although evolution runs forever, we are constrained by compute and probably want
     our programs to stop, so define when a state is good enough.
     """
 
-    fn: Callable[[State], bool]
 
-    @staticmethod
-    def null(_: State) -> bool:
-        return False
-
-
-@dataclass(frozen=True)
-class Selector(_Named):
+class Selector(Component[Callable[[list[State], list[State]], list[State]]]):
     """What states in a population are maintained?
 
     The first argument to this class's function is the parents' states, and the second
@@ -136,26 +146,20 @@ class Selector(_Named):
     limited by carrying capacity.
     """
 
-    fn: Callable[[list[State], list[State]], list[State]]
 
-    @staticmethod
-    def null(parents: list[State], offspring: list[State]) -> list[State]:
-        return parents + offspring
-
-
-@dataclass(frozen=True)
-class Environment(_Named):
+@dataclass(frozen=True, slots=True)
+class Environment(Component[NoneType]):
     """Specification of components: what is the algorithm?"""
 
-    recombinator: Recombinator = Recombinator("Null", Recombinator.null)
-    mutator: Mutator = Mutator("Null", Mutator.null)
-    fitness: Fitness = Fitness("Null", Fitness.null)
-    goal_test: GoalTest = GoalTest("Null", GoalTest.null)
-    selector: Selector = Selector("Null", Selector.null)
+    recombinator: Recombinator
+    mutator: Mutator
+    fitness: Fitness
+    goal_test: GoalTest
+    selector: Selector
 
 
-@dataclass(frozen=True)
-class Parameter(_Named):
+@dataclass(frozen=True, slots=True)
+class Parameter(Component[NoneType]):
     """Specification of tuneable parameters: how hard should the algorithm try?
 
     We want to know how many offspring to produce per generation, how many generations
@@ -167,6 +171,44 @@ class Parameter(_Named):
     reproductions: int = 10
     max_generations: int = 10
     initial_pop_size: int = 10
+
+
+## Component construction. #############################################################
+
+
+@Component.to(Recombinator)
+def halfway_split(a: State, b: State) -> State:
+    split = Context.dimensions // 2
+    return State.of(a.data[:split] + b.data[split:])
+
+
+@Component.to(Recombinator)
+def random_split(a: State, b: State) -> State:
+    split = randint(1, Context.dimensions - 1)
+    return State.of(a.data[:split] + b.data[split:])
+
+
+@Component.to(Mutator)
+def bounded_nudge(state: State) -> State:
+    position = randint(0, Context.dimensions - 1)
+    new_value = state.data[position] + choice([-1, 1])
+    if new_value not in range(Context.dimensions):
+        new_value = state.data[position]
+    return State.of((*state.data[:position], new_value, *state.data[position + 1 :]))
+
+
+@Component.to(Mutator)
+def wrapped_nudge(state: State) -> State:
+    position = randint(0, Context.dimensions - 1)
+    new_value = (state.data[position] + choice([-1, 1])) % Context.dimensions
+    return State.of((*state.data[:position], new_value, *state.data[position + 1 :]))
+
+
+@Component.to(Mutator)
+def regenerate(state: State) -> State:
+    position = randint(0, Context.dimensions - 1)
+    new_value = randint(0, Context.dimensions - 1)
+    return State.of((*state.data[:position], new_value, *state.data[position + 1 :]))
 
 
 ## Algorithm definition. ###############################################################
@@ -185,22 +227,19 @@ def genetic_search(
     parents = [State.random() for _ in range(parameter.initial_pop_size)]
     while True:
         for state in parents:
-            if environment.goal_test.fn(state):
+            if environment.goal_test.data(state):
                 return generation, state
         if parameter.max_generations > 0 and generation >= parameter.max_generations:
             return generation, None
-        fitnesses = map(environment.fitness.fn, parents)
+        fitnesses = map(environment.fitness.data, parents)
         pairs = [
             choices(parents, list(fitnesses), k=2)
             for _ in range(parameter.reproductions)
         ]
-        children = (environment.recombinator.fn(*p) for p in pairs)
-        children = map(environment.mutator.fn, children)
-        parents = environment.selector.fn(parents, list(children))
+        children = (environment.recombinator.data(*p) for p in pairs)
+        children = map(environment.mutator.data, children)
+        parents = environment.selector.data(parents, list(children))
         generation += 1
-
-
-## Environment and parameter examples. #################################################
 
 
 ## Driver. #############################################################################
@@ -226,28 +265,36 @@ def drive() -> None:
     print(f"Running {total_tests} tests, each of {Context.trials} trials...")
     for environment in environments:
         for parameter in parameters:
-            print(f"Test {curr_test}")
+            print(f"Test {curr_test}... ", end="")
             print(environment)
             print(parameter)
-            times = [do_trial(i, environment, parameter) for i in range(Context.trials)]
-            print(f"\tAverage time: {sum(times) / len(times):.2f} seconds.")
+            solves, times = [
+                do_trial(i, environment, parameter) for i in range(Context.trials)
+            ]
+            time = sum(times) / len(times)
+            solve = sum(solves) / len(solves)
+            print(f"average {time} seconds, {int(100 * solve)}% success rate.")
             curr_test += 1
 
 
-def do_trial(number: int, environment: Environment, parameter: Parameter) -> float:
+def do_trial(
+    number: int,
+    environment: Environment,
+    parameter: Parameter,
+) -> tuple[bool, float]:
     start_time = time()
     generations, solution = genetic_search(environment, parameter)
     end_time = time()
-    print(f"\tTrial {number}: ", end="")
+    debug(f"Trial {number}: ")
     print(f"{generations} generations." if solution else "Failed.", end="")
     print(f"{end_time - start_time:.2f} seconds.")
-    return end_time - start_time
+    return bool(solution), end_time - start_time
 
 
 ## Frontend. ###########################################################################
 
 
-def parse_args(argv: list[str]) -> None:
+def parse_args(argv: list[str]) -> bool:
     if len(argv) < 1:
         msg = "Must provide (or spoof) __name__ as argv[0]."
         raise ValueError(msg)
@@ -292,10 +339,12 @@ def parse_args(argv: list[str]) -> None:
     Context.debug = args.debug  # pyright: ignore[reportAny]
     Context.seed = args.seed  # pyright: ignore[reportAny]
     Context.trials = args.trials  # pyright: ignore[reportAny]
+    return Context.validate()
 
 
 def main(argv: list[str]) -> int:
-    parse_args(argv)
+    if not parse_args(argv):
+        return 1
     drive()
     return 0
 
